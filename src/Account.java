53917ef3ce424cf56774f41e02f96ee93408ea5e
Melanie Mai <melaniesmai@outlook.com>
2020-11-24 21:24:25 -0500

    Updated access

--- src/Account.java
+++ src/Account.java
@@ -2,13 +2,13 @@ public abstract class Account implements IBaseRate {
     // Will not be able to create objects from the Account class -> parent class
 
     // Common properties between savings and checking account
-    String name;
-    String ssn;
-    double balance;
-    String accountNum;
-    double rate;
+    private String name;
+    private String ssn;
+    private double balance;
+    protected String accountNum;
+    protected double rate;
 
-    static int index = 10000;
+    private static int index = 10000;
 
     // Constructor, initialize account
     public Account(String name, String sSN, double initDep) {
