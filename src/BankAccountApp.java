331297fa6ba0d7f8ad8f7c5263a6adaf02442092
Melanie Mai <melaniesmai@outlook.com>
2020-11-24 21:25:18 -0500

    Testing

--- src/BankAccountApp.java
+++ src/BankAccountApp.java
@@ -1,15 +1,28 @@
+import java.util.List;
+
 public class BankAccountApp {
     public static void main(String[] args) throws Exception {
-        Checking checkingAccount = new Checking("Tom Wilson", "358955700", 1500);
-        Savings savingsAccount = new Savings("Suzy Low", "796821570", 2500);
+        String file = "/Users/melaniemai/Downloads/NewBankAccounts.csv";
 
-        savingsAccount.showInfo();
-        System.out.println("***********");
-        checkingAccount.showInfo();
-
-        savingsAccount.deposit(3000);
+        /*
+         * Checking checkingAccount = new Checking("Tom Wilson", "358955700", 1500);
+         * Savings savingsAccount = new Savings("Suzy Low", "796821570", 2500);
+         * 
+         * savingsAccount.showInfo(); System.out.println("***********");
+         * checkingAccount.showInfo();
+         * 
+         * savingsAccount.compound();
+         */
 
         // Read .cvs file
+        List<String[]> newAccounts = CSV.read(file);
+        for (String[] accountHolder : newAccounts) {
+            System.out.println("NEW ACCOUNT HOLDER:");
+            System.out.println(accountHolder[0]);
+            System.out.println(accountHolder[1]);
+            System.out.println(accountHolder[2]);
+            System.out.println(accountHolder[3]);
+        }
 
         // Create new accounts based on data
     }
